019300ebb2a42db3dea20d9f5d4efa7e9b85a708
Luis Miño B <l.mino.bustos@gmail.com>
2023-09-15 00:29:08 -0300

    feat: Jobs scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :jobs
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'jobs#index'
 end
