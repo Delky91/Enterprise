4d06563d71a3fae5d3d7763087cfc90f1acaf536
Luis Miño B <l.mino.bustos@gmail.com>
2023-09-23 23:46:32 -0300

    fixed styles in jobs update and show_user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'aws-sdk-s3'
+
+Aws.config.update(
+  region: ENV['aws_bucket_region'],
+  credentials: Aws::Credentials.new(
+    ENV['aws_access_key_id'],
+    ENV['aws_secret_access_key']
+  )
+)
+
+admin_user = User.create(
+  name: 'Admin User',
+  email: 'admin@mail.com',
+  password: '123456',
+  role: 'admin'
+)
+admin_user.profile_picture.attach(
+  io: URI.open('https://rails-luis-bucket.s3.us-east-2.amazonaws.com/mlz596kreencfhad2aw31jvd2jva'),
+  filename: 'profile_picture.jpg'
+)
